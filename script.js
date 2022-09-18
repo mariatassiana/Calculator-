736919f61c2cf1d45b86509bb17ec2cee01ad3a3
Maria Tassiana <mariatassiana1@gmail.com>
2022-09-18 20:18:37 -0300

    finalizando com javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+function calc(op){
+
+    num1 = parseFloat(document.getElementById("num1").value)
+    num2 = parseFloat(document.getElementById("num2").value)
+    var result = 0;
+
+    switch(op)
+    {
+        case '+': result  = num1 + num2; break;
+        case '-': result  = num1 - num2; break;
+        case '*': result  = num1 * num2; break;
+        case '/': result  = num1 / num2; break;
+    }
+
+    document.getElementById("output").value = result;
+
+    var newOutput = num1 + " " + op + " " + num2 + " = " +result;
+
+    document.getElementById("history").innerHTML += "<p>" + newOutput + "</p>";
+    
+    var list = document.getElementById("history");
+    if(list.children.length > 10){
+        document.getElementById("history").removeChild(list.childNodes[0]);    
+    }
+    
+
+}
+
+
